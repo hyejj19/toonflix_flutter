bda40da4dabd56a5ae394616eb796f31817f4c3d
hyejj19@naver.com <hyejj19@naver.com>
2024-08-25 00:03:27 +0900

    [Add] Header

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,14 +8,39 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello world'),
-        ),
-      ),
-    );
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.9), fontSize: 18),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          )),
+    ));
   }
 }
