afcbcf325f7e51e5047bd6244e011bbdfce60f07
hyejj19@naver.com <hyejj19@naver.com>
2025-01-09 22:51:13 +0900

    #5.1 Timer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSecs = 1500;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSecs -= 1;
+    });
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Container(
                 alignment: Alignment.bottomCenter,
                 child: Text(
-                  '25:00',
+                  '$totalSecs',
                   style: TextStyle(
                     color: Theme.of(context).cardColor,
                     fontSize: 90,
@@ -36,7 +51,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     size: 120,
                     color: Theme.of(context).cardColor,
                   ),
-                  onPressed: () {},
+                  onPressed: onStartPressed,
                 ),
               )),
           Flexible(
@@ -47,6 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Container(
                       decoration: BoxDecoration(
                         color: Theme.of(context).cardColor,
+                        borderRadius: BorderRadius.circular(50),
                       ),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
